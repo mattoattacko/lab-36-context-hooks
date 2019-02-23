9e0c4d2311622d005469b9df79253eb1151e267d
Matthew McQuain <37052988+mattoattacko@users.noreply.github.com>
2019-02-22 19:12:46 -0800

    added header because reasons

---
+++ src/header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>Seahawks Fan Counter</h1>
+        <p>push the + button to make the fan counter go up!</p>
+      </header>
+    );
+  }
+}
+
+export default Header;
