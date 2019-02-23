c6d7a156de40ce790b0317e01d9e00f6bf6b4cab
Matthew McQuain <37052988+mattoattacko@users.noreply.github.com>
2019-02-22 18:54:07 -0800

    transferring files

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import SettingsContext from "./components/settings/counter-context.js";
+import Decrementer from "./components/decrementer/decrementer.js";
+import Counter from "./components/counter/counter.js";
+import Incrementer from "./components/incrementer/incrementer.js";
+
+import "./app.scss";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <SettingsContext>
+        <section className="main-section">
+          <Decrementer />
+          <Counter />
+          <Incrementer />
+        </section>
+      </SettingsContext>
+    );
+  }
+}
