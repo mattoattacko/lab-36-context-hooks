a627efe0235c43631c58b8f1bbef8347198d3c27
Matthew McQuain <37052988+mattoattacko@users.noreply.github.com>
2019-02-22 19:07:26 -0800

    styling fixes

--- src/app.js
+++ src/app.js
@@ -1,10 +1,8 @@
 import React from "react";
-
 import SettingsContext from "./components/settings/counter-context.js";
 import Decrementer from "./components/decrementer/decrementer.js";
 import Counter from "./components/counter/counter.js";
 import Incrementer from "./components/incrementer/incrementer.js";
-
 import "./app.scss";
 
 export default class App extends React.Component {
